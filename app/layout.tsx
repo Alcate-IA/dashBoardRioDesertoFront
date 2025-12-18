b95715222b144ef0a4dbf533bd86c33441e4a2f1
matheusilveiraw <matheus.silveiraw@gmail.com>
2025-12-18 12:04:18 -0300

    componentes em português

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { LayoutProvider } from '../layout/context/layoutcontext';
 import { PrimeReactProvider } from 'primereact/api';
+import { addLocale, locale } from 'primereact/api';
 import 'primereact/resources/primereact.css';
 import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
@@ -14,8 +15,28 @@ interface RootLayoutProps {
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
+    // Configurar locale em português brasileiro
+    addLocale('pt-BR', {
+        firstDayOfWeek: 0,
+        dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+        dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
+        dayNamesMin: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
+        monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+        monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+        today: 'Hoje',
+        clear: 'Limpar',
+        accept: 'Aceitar',
+        reject: 'Rejeitar',
+        choose: 'Escolher',
+        upload: 'Enviar',
+        cancel: 'Cancelar',
+        fileSizeTypes: ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+    });
+
+    locale('pt-BR');
+
     return (
-        <html lang="en" suppressHydrationWarning>
+        <html lang="pt-BR" suppressHydrationWarning>
             <head>
                 <link id="theme-css" href={`/themes/lara-dark-amber/theme.css`} rel="stylesheet"></link>
             </head>
